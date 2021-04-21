7885435ae680eba948c271a707cfb9b666e5a3a9
ggBean <doci1803@naver.com>
2021-04-21 16:58:56 +0900

    파이썬 자동 자가진단

---
+++ main.py
@@ -0,0 +1,25 @@
+import hcskr
+from hcskr import selfChk
+
+
+name = input('이름을 입력해주세요. ')
+birth = input('생년월일을 입력해주세요. ')
+pw = input('비밀번호를 입력해주세요. ')
+
+selfChk(name, birth, pw)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+sdd
+24546568798009
